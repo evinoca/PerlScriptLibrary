33a130be2c5c4194d4e72147faafa98bacc6b2eb
Audric <evinoca@gmail.com>
2015-09-17 17:16:05 +0800

    update

--- Text Procedure/QA_Daily_Report.pl
+++ Text Procedure/QA_Daily_Report.pl
@@ -1,25 +1,31 @@
 #!/usr/perl
-
 use utf8;
 use strict;
 use File::stat;
 use File::Copy;
-use Net::Telnet;
 use File::Basename;
+use Data::Dumper;
+use Encode;
 
 #获取系统路径并切换路径
 my $work_path =  dirname(__FILE__);
-#print $work_path."\n";
-
 chdir $work_path or die $!;
+
+opendir(DIR, $work_path."//DailyReport") or die $!;
+my @filename = readdir(DIR);
+#print Dumper(@filename);
+@filename= sort {$a cmp $b} @filename; 
+#print Dumper(@filename);
+my $archiveFolder = "Archived";
+
+#my %teamOrg = ('AT',"Automation",'ST',"System_Test",'MT',"Mobile_Test");
+my %teamOrg = ('AT',"Automation",'ST',"System_Test");
 
 sub getTime
 {
    #time()函数返回从1970年1月1日起累计秒数
     my $time = shift || time();
-
     my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst,$monF) = localtime($time);
-
     $mon ++;
     $sec  = ($sec<10)?"0$sec":$sec;#秒数[0,59]
     $min  = ($min<10)?"0$min":$min;#分数[0,59]
@@ -27,7 +33,6 @@ sub getTime
     $mday = ($mday<10)?"0$mday":$mday;#这个月的第几天[1,31]
     $monF  = ($mon<10)?"0$mon":$mon;#月数[0,11],要将$mon加1之后，才能符合实际情况。
     $year+=1900;
-
     my $weekday = ('Sun','Mon','Tue','Wed','Thu','Fri','Sat')[$wday];
     return { 'second' => $sec,
              'minute' => $min,
@@ -46,44 +51,41 @@ sub getTime
 
 #main
 my $date = &getTime();#获取当前系统时间的Hash
-my $timestaemp = $date->{dateF};#获取yyyymmdd这样的日期
+my $timestamp = $date->{dateF};#获取yyyymmdd这样的日期
 my $timeRegexpString= $date->{date};
-my $reportFileName_ST= "QA_System_Test_DailyReport_$timestaemp.txt";
-my $reportFileName_AT= "QA_Automation_DailyReport_$timestaemp.txt";
-
-
-open(REPORT_ST,">".$reportFileName_ST) or die $!;
-open(REPORT_AT,">".$reportFileName_AT) or die $!;
-
 my %detail;
+my %counter;
 
-my $st = 0;
-my $at=0;
-my $st_profile = 0;
-my $at_profile = 0;
-
+foreach my $t (keys %teamOrg)
+{
+	$counter{$t}{'updated'}=0;
+	$counter{$t}{'profile'}=0;
+	$counter{$t}{'ReportFile'}="QA_".$teamOrg{$t}."_DailyReport_$timestamp.txt";
+	open($counter{$t}{'fileHandler'},">".$counter{$t}{'ReportFile'}) or die $!;
+}
+#print Dumper(%counter);
 chdir $work_path."//DailyReport" or die "Can't find path DailyReport".$!;
 
-foreach my $file (glob "*.txt")
+#foreach my $file (glob "*.txt")
+foreach my $file (@filename)
 	{
 		#print $file;
-		if ($file=~m/QA/)
-		{
-		}
-		elsif ($file=~m/ST_(.*).txt/)
-		{
+		if ($file=~m/(.*)_(.*).txt/)
+		{		
+			my $team = $1;
+			my $teamMember = $2;
+			next if not exists($teamOrg{$team});
+			my $teamFullName = $teamOrg{$1};
 			my $rwFlag= 0;
 			my $submitFlag = 0;
-			$st_profile++;
-			$detail{$1}{"team"}="System Test";
-			syswrite(REPORT_ST,$1." @ ") or die "Can't write report file".$!;
+			$counter{$team}{'profile'}++;
+			syswrite($counter{$team}{'fileHandler'},$teamMember." @ ") or die "Can't write report file".$!;
 			open FILE, $file or die $!;
 			chomp (my @lines = (<FILE>));
 			close(FILE);
-			my $i = 1;
+			
 			foreach my $line (@lines)
 			{
-			   #print $line."\n";
 			   if ($line=~m/$timeRegexpString/)
 			   {
 					$rwFlag= 1;
@@ -94,63 +96,52 @@ foreach my $file (glob "*.txt")
 			   }
 			   if ($rwFlag== 1)
 			   {
-					syswrite(REPORT_ST,$line."\r\n") or die "Can't write report file".$!;
+					syswrite($counter{$team}{'fileHandler'},$line."\r\n") or die "Can't write report file".$!;
 					$submitFlag=1;
 			   }				
 			}
-			if ($submitFlag == 0){
-				$detail{$1}{"status"}="Not Submit";
-				syswrite(REPORT_ST,"\r\n") or die "Can't write report file".$!;
-			}
-			else{
-				$st++;
-				$detail{$1}{"status"}="Updated";			
-			}
-			syswrite(REPORT_ST,"\r\n") or die "Can't write report file".$!;
-		}
-		elsif ($file=~m/AT_(.*).txt/)
-		{
-			my $rwFlag= 0;
-			my $submitFlag =0;
-			$at_profile++;
-			$detail{$1}{"team"}="Automation Test";
-			syswrite(REPORT_AT,$1." @ ") or die "Can't write report file".$!;
-			open FILE, $file or die $!;
-			chomp (my @lines = (<FILE>));
-			close(FILE);
-			my $i = 1;
-			foreach my $line (@lines)
+			if ($submitFlag == 0)
 			{
-			   #print $line."\n";
-			   if ($line=~m/$timeRegexpString/)
-			   {
-					$rwFlag= 1;
-			   }
-			   elsif($line=~m/\d{4}\/\d?\/\d?/)
-			   {
-					$rwFlag= 0;
-			   }
-			   if ($rwFlag== 1)
-			   {
-					syswrite(REPORT_AT,$line."\r\n") or die "Can't write report file".$!;
-			   }
+				$detail{"NotUpdate"}{$teamMember}{"status"}="Not Submit";
+				$detail{"NotUpdate"}{$teamMember}{"team"}=$teamOrg{$team};
+				syswrite($counter{$team}{'fileHandler'},"\r\n") or die "Can't write report file".$!;
 			}
-			if ($submitFlag == 0){
-				$detail{$1}{"status"}="Not Submit";
-				syswrite(REPORT_AT,"\r\n") or die "Can't write report file".$!;
+			else
+			{
+				$counter{$team}{'updated'}++;
+				$detail{"Updated"}{$team}{$teamMember}{"status"}="Updated";	
+				$detail{"Updated"}{$team}{$teamMember}{"team"}=$teamOrg{$team};
 			}
-			else{
-				$at++;
-				$detail{$1}{"status"}="Updated";
-			}
-			syswrite(REPORT_AT,"\r\n") or die "Can't write report file".$!;
+			syswrite($counter{$team}{'fileHandler'},"\r\n") or die "Can't write report file".$!;
 		}
+		else
+		{
+		next;
+			}
 	}
-	
-my ($dname, $dteam, $dstatus);	
-foreach my $name (keys %detail)
- {
-	($dname, $dteam, $dstatus)= ($name,$detail{$name}{"team"},$detail{$name}{"status"});
+#print Dumper(%detail);	
+my ($dname, $dteam, $dstatus);
+my $updatedHash = $detail{"Updated"};
+my $notUpdateHash = $detail{"NotUpdate"};
+my $updated_size=keys%$updatedHash;
+my $notupdated_size = keys%$notUpdateHash;
+#print Dumper($updatedHash);
+
+foreach my $teamHash (sort {$b cmp $a} keys %$updatedHash){
+	my $updateTeamHash =  $detail{"Updated"}{$teamHash};
+	#print Dumper($updateTeamHash);
+	foreach my $name (sort {$b cmp $a} keys %$updateTeamHash)
+	{
+		($dname, $dteam, $dstatus)= ($name,$detail{"Updated"}{$teamHash}{$name}{"team"},$detail{"Updated"}{$teamHash}{$name}{"status"});
+		$^ = 'STDOUT_TOP';
+		$~ = 'STDOUT';
+		write or die "Error",$!;
+	}
+}
+print "------------------------------------------------------------------\n"  if ($updated_size!=0 or $notupdated_size!=0);
+foreach my $name (sort {$b cmp $a} keys %$notUpdateHash)
+{
+	($dname, $dteam, $dstatus)= ($name,$detail{"NotUpdate"}{$name}{"team"},$detail{"NotUpdate"}{$name}{"status"});
 	$^ = 'STDOUT_TOP';
 	$~ = 'STDOUT';
 	write or die "Error",$!;
@@ -169,10 +160,27 @@ format STDOUT =
 @<<<<<<<<<<<<<@<<<<<<<<<<<<<<<<<<@<<<<<<<<<<<<<<<<<<
 $dname,                        $dteam,                                   $dstatus
 .
-
-close(REPORT_ST) or die $!;
-close(REPORT_AT)or die $!;
 print "------------------------------------------------------------------\n";
-print "ST: ".$st."/".$st_profile."\n";
-print "AT: ".$at."/".$at_profile."\n";
+foreach my $t (keys %teamOrg)
+{
+	close($counter{$t}{'fileHandler'});
+	print $t,": ",$counter{$t}{"updated"},"/",$counter{$t}{"profile"},"\n";
+}
+print "------------------------------------------------------------------\n";
+
+
+chdir $work_path or die $!;
+mkdir $archiveFolder unless -e $archiveFolder;
+
+opendir(DIR, $work_path) or die $!;
+my @filename = readdir(DIR);
+foreach my $file (@filename)
+{
+	if ($file=~m/^QA.*.txt/){
+	#print $file."\n";
+	my $source = $work_path ."\\".$file;
+	my $dest = $work_path ."\\".$archiveFolder."\\".$file;
+	move ($work_path ."/".$file,$dest)  unless $file =~m/$timestamp/;
+	}	
+}
 system("pause");
